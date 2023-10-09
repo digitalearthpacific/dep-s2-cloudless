04104e84d0d9ae1c3937e19e829da1f0253d9f48
Sachindra Singh <sachindras@spc.int>
2023-10-09 16:10:46 +1200

    Print Tasks Fix.

--- src/print_tasks.py
+++ src/print_tasks.py
@@ -8,7 +8,7 @@ from azure_logger import CsvLogger, filter_by_log
 from dep_tools.namers import DepItemPath
 from dep_tools.azure import get_container_client
 
-from run_task import BASE_PRODUCT, DATASET_ID, get_areas
+from grid import grid
 
 
 def main(
@@ -17,9 +17,8 @@ def main(
     version: Annotated[str, typer.Option()],
     limit: Optional[str] = None,
     no_retry_errors: Optional[bool] = False,
-    dataset_id: str = DATASET_ID,
+    dataset_id: str = "wofs",
 ) -> None:
-    assert dataset_id is not None, "dataset_id must be provided"
     region_codes = None if regions.upper() == "ALL" else regions.split(",")
 
     # Makes a list no matter what
@@ -27,14 +26,13 @@ def main(
     if len(years) == 2:
         years = range(int(years[0]), int(years[1]) + 1)
     elif len(years) > 2:
-        raise ValueError(f"{datetime} is not a valid value for --datetime")
+        ValueError(f"{datetime} is not a valid value for --datetime")
 
-    areas = get_areas()
     grid_subset = (
-        areas.loc[areas.code.isin(region_codes)] if region_codes is not None else areas
+        grid.loc[grid.code.isin(region_codes)] if region_codes is not None else grid
     )
 
-    itempath = DepItemPath(BASE_PRODUCT, dataset_id, version, datetime)
+    itempath = DepItemPath("ls", dataset_id, version, datetime)
     logger = CsvLogger(
         name=dataset_id,
         container_client=get_container_client(),
